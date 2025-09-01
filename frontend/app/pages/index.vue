d6e77dc076673f0a3da91fd39936badf15c80980
tobinio <Tobias.frischmann1@gmail.com>
2025-09-01 20:02:38 +0200

    add simple frontend

---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const hexColor = ref("#000000");
+</script>
+<template>
+    <div class="bg-neutral-800 h-dvh p-4">
+        <div class="text-white">
+            {{ hexColor }}
+        </div>
+        <HueSlider v-model:hex-color="hexColor" />
+    </div>
+</template>
